9bdad4a89878ad9038f420fc0197c147644791d1
armandjedilberto <47874640+armandjedilberto@users.noreply.github.com>
2019-03-19 19:35:46 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2
+{
+    class Program
+    {
+        int numero1, numero2;
+       public static void Main(string[] args)
+        {
+            
+            obj = new obj numero1;
+            obj = new obj numero2;
+    
+            Console.WriteLine("ingrese el primer numero " + numero1());
+            Console.WriteLine("ingrese el segundo numero" + numero2());
+           {
+               Console.ReadKey(numero1/numero2);
+           }
+
+       }
+    }
+}
